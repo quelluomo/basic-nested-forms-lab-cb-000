8a77178740b9ec1bc78048b079348dc10c00ef11
Matteo Muscas <thatman.j@gmail.com>
2019-06-12 07:47:12 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     @recipe.ingrendients.build(name: 'work')
+    @recipe.ingrendients.build(quantity: '0')
   end
 
   def create
